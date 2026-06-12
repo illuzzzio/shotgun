a23fc76eb15fc27ca77e2f2ffe9a7352609432c4
Pranjal Malhotra <152037759+illuzzzio@users.noreply.github.com>
2026-06-12 14:16:03 +0530

    collections counter method for valid anagram

--- best_150/leetcode_242_valid_anagram.py
+++ best_150/leetcode_242_valid_anagram.py
@@ -5,3 +5,16 @@ def valid_anagram(s,t):
     return sorted(s)== sorted(t)
 
 print(valid_anagram(s,t))   # O(nlogn)
+
+
+from collections import Counter 
+def valid_anagram_2(s,t):
+
+    if(len(s)!= len(t)):
+        return False 
+    s_dict = Counter(s)
+    t_dict = Counter(t)
+    return s_dict==t_dict
+
+
+print(valid_anagram_2(s,t))
