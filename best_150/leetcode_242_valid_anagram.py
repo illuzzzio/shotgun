643ba04e3a9f122c2c52804904d75887861fc041
Pranjal Malhotra <152037759+illuzzzio@users.noreply.github.com>
2026-06-12 14:31:51 +0530

    bestway for valid anagram and learning get synta with key and default value

--- best_150/leetcode_242_valid_anagram.py
+++ best_150/leetcode_242_valid_anagram.py
@@ -18,3 +18,21 @@ def valid_anagram_2(s,t):
 
 
 print(valid_anagram_2(s,t))
+
+
+def best_valid_anagram(s,t):
+    if len(s)!=len(t):
+        return False 
+    counter_dict = {}
+    for ch in s:
+        counter_dict[ch] = counter_dict.get(ch, 0) +1  # get syntx .get(key, defult value ) suppose in age doesnt exist in dict it will show none but if we use get it will show 0
+    for ch in t:
+        counter_dict[ch] = counter_dict.get(ch,0) -1
+
+    for value in counter_dict.values():
+        if value!= 0:
+            return False 
+    return True 
+
+print(best_valid_anagram(s,t))
+
